f565cc039e25095d59dffa0169228f07a23b7f66
Khanhnv99 <nhoxkonga2006@gmail.com>
2023-09-24 17:32:46 +0700

    xu ly logic phần search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -3,7 +3,6 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleQuestion,
-    faCloudUpload,
     faCoins,
     faEarthAsia,
     faEllipsisVertical,
